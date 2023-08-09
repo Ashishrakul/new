7998c3fc1dd299f8ce1032fa79891b11fdb2d942
Ashishrakul <141629292+Ashishrakul@users.noreply.github.com>
2023-08-09 12:43:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+function MyButton() {
+    return (
+      <button>
+        I'm a button
+      </button>
+    );
+  }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        
+        <h1>Welcome to my app</h1>
+        <MyButton />
+        <img src="https://th.bing.com/th?id=OIP.E4IJcali_762Oo_vNhhbFgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"></img>
+        
+      </div>
+    
+    );
+    }
+
+  
